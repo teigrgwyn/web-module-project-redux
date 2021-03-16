972bf76670b794734a72ad769033d06df29c7253
Cory Young <teigrgwyn@gmail.com>
2021-03-16 15:25:50 -0500

    fixed sass bug (failed to install to use files, reverted to default css)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useReducer } from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
@@ -7,11 +9,14 @@ import Total from './components/Total';
 
 import reducer, { initialState } from './reducers/index';
 import { addFeature, removeFeature } from './actions/index';
-
+ 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const store = createStore(reducer);
 
   return (
+    <Provider store={store}>
+
     <div className="boxes">
       <div className="box">
         <Header car={state.car} />
@@ -22,6 +27,8 @@ const App = () => {
         <Total car={state.car} additionalPrice={state.additionalPrice} />
       </div>
     </div>
+
+    </Provider>
   );
 };
 
